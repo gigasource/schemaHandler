9dfa3faa7b36f9839868244c4d5f822614265752
Pham Anh Tuan <tuanpa2295@gmail.com>
2020-12-02 15:15:39 +0700

    update commitPlugin code

---
+++ plugins/commitPlugin.js
@@ -0,0 +1,114 @@
+const Queue = require('better-queue')
+const ObjectID = require('bson').ObjectID
+const jsonFn = require('json-fn')
+const _ = require('lodash')
+
+const TAG = require('./tags').COMMIT_LAYER_TAG
+const { initTransporterWithOrm } = require('./transporter')
+const TRANSFORM_LAYER_TAG = require('./tags').TRANSPORT_LAYER_TAG
+const allowedFn = [] // todo: fill this
+
+module.exports = function (orm) {
+  const queue = new Queue(async function (commits, cb) {
+    if (!Array.isArray(commits)) commits = [commits]
+    for (let commit of commits) {
+      await orm.emit(`commit:${commitTypes[commit.collectionName].commitType}`, commit)
+    }
+    cb()
+  })
+  const commitTypes = {}
+  let isMaster, setMaster
+
+  if (orm.mode === 'single') {
+    isMaster = false
+    setMaster = (_isMaster) => {
+      isMaster = _isMaster
+    }
+  } else {
+    isMaster = {}
+    setMaster = (dbName, _isMaster) => {
+      isMaster[dbName] = _isMaster
+    }
+  }
+  const getMaster = (dbName) => {
+    return dbName ? orm.commitHandler.isMaster[dbName] : orm.commitHandler.isMaster
+  }
+
+  orm.onDefault(`${TAG}:sync`, (commits) => {
+    queue.push(commits)
+  })
+  queue.pause()
+
+  const registerCommitCollections = function (commitTypeGroups) {
+    for (let commitType in commitTypeGroups) {
+      commitTypeGroups[commitType].forEach(collection => {
+        if (typeof collection === 'string') {
+          collection = {
+            name: collection,
+            commitType
+          }
+          const collectionName = collection.name
+          commitTypes[collectionName] = collection
+        }
+      })
+      startQueue()
+    }
+    return commitTypes
+    // collectionsList.forEach(collection => {
+    //   if (typeof collection === 'string') {
+    //     collection = {
+    //       name: collection,
+    //       needMaster: true,
+    //       groupName
+    //     }
+    //   }
+    //   const collectionName = collection.name
+    //   if (commitTypes[collectionName]) {
+    //     console.warn(`Collection ${collectionName} is registered more than one time`)
+    //   }
+    //   commitTypes[collectionName] = collection
+    // })
+    // return true
+  }
+
+  const startQueue = function () {
+    queue.resume()
+  }
+
+  const createCommit = function (query, tags, data) {
+    return {
+      tags,
+      data,
+      _id: new ObjectID(),
+      query: jsonFn.stringify(query),
+      collectionName: query.name
+    }
+  }
+
+  orm.on('pre:execChain', async function (query) {
+    if (!commitTypes[query.name]) return
+    // todo check allowed fn
+    const last = _.last(query.chain)
+    let tags = []
+    let data = {}
+    if (last.fn === 'commit') {
+      query.chain.pop()
+      const { args } = last;
+      tags = args.filter(arg => typeof arg === 'string')
+      data = _.assign({}, ...args.filter(arg => typeof arg === 'object'))
+    }
+    const commit = createCommit(query, tags, data)
+    const dbName = (orm.mode === 'single' ? undefined : query.name.split('@')[1])
+    const _isMaster = (orm.mode === 'single' ? isMaster : isMaster[dbName])
+    await orm.emit(`${TRANSFORM_LAYER_TAG}:sync`, commit, dbName, _isMaster)
+    this.ok = true
+  })
+
+  Object.assign(orm, {
+    getMaster,
+    registerCommitCollections,
+    setMaster
+  })
+}
+
+module.exports.TAG = TAG
---
+++ plugins/transporter.js
@@ -0,0 +1,61 @@
+let clientSocket
+let masterSocket
+
+const TAG = require('./tags').TRANSPORT_LAYER_TAG
+const COMMIT_LAYER_TAG = require('./tags').COMMIT_LAYER_TAG
+
+module.exports = function (orm) {
+  orm.on('externalSocketConnected', socket => {
+    onlineOrderSocket = socket
+  })
+  // for master socket
+  orm.on(`${TAG}:registerClientSocket`, (_clientSocket, dbName) => {
+    _clientSocket.on('requireSync', async () => {
+      await orm.emit(`${COMMIT_LAYER_TAG}:requireSync`, ...arguments, dbName)
+    })
+    _clientSocket.on('sync', async () => {
+      await orm.emit(`${COMMIT_LAYER_TAG}:sync`, ...arguments, dbName)
+    })
+    _clientSocket.on('nodeCall', async () => {
+      await orm.emit(`${COMMIT_LAYER_TAG}:nodeCall`, ...arguments, dbName)
+    })
+    const emitToAllCb = (commits, _dbName) => {
+      if (_dbName !== dbName) return
+      _clientSocket.emit('sync', commits, dbName)
+    }
+    _clientSocket.on('disconnect', () => {
+      orm.off(`${TAG}:emitToAll`, emitToAllCb)
+    })
+    orm.on(`${TAG}:emitToAll`, emitToAllCb)
+  })
+  // for node socket
+  orm.on(`${TAG}:registerMasterSocket`, (_masterSocket, dbName) => {
+    _masterSocket.on('sync', async function () {
+      await orm.emit(`${COMMIT_LAYER_TAG}:sync`, ...arguments, dbName)
+    })
+    _masterSocket.on('masterCall', async function () {
+      await orm.emit(`${COMMIT_LAYER_TAG}:masterCall`, ...arguments, dbName)
+    })
+    const syncCb = (commits, _dbName) => {
+      if (_dbName !== dbName) return
+      _masterSocket.emit('sync', commits, dbName)
+    }
+    _masterSocket.on('disconnect', function () {
+      orm.off(`${TAG}:emitToMaster`, syncCb)
+    })
+    orm.on(`${TAG}:emitToMaster`, syncCb)
+  })
+  // for both socket
+  orm.on(`${TAG}:sync`, async function (commits, dbName, isMaster) {
+    if (typeof dbName === 'boolean') {
+      isMaster = dbName
+    }
+    if (isMaster) {
+      await orm.emit(`${COMMIT_LAYER_TAG}:sync`, commits)
+    } else {
+      await orm.emit(`${TAG}:emitToMaster`, commits, dbName)
+    }
+  })
+}
+
+module.exports.TAG = TAG
